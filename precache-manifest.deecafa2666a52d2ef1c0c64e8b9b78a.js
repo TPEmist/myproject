ed22002f6354d59f3098e9e8fa4a46dd0f1cadaf
TPEmist <you@example.com>
2019-04-15 20:59:58 +0800

    Updates

---
+++ precache-manifest.deecafa2666a52d2ef1c0c64e8b9b78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myproject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f8ccf630f8fb1fa091c",
+    "url": "/myproject/static/js/runtime~main.1e800c5e.js"
+  },
+  {
+    "revision": "a6a952466e0b5809b8fd",
+    "url": "/myproject/static/js/main.21508954.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/myproject/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a6a952466e0b5809b8fd",
+    "url": "/myproject/static/css/main.15393cb7.chunk.css"
+  },
+  {
+    "revision": "561b368e53dd975f87b226f56b28dc74",
+    "url": "/myproject/index.html"
+  }
+];
